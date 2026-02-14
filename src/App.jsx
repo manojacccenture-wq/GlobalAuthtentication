d7fc4c5efdd0de41506098f2c62ea8b9eea3a637
debugmanoj <manojkumarwork01@gmail.com>
2026-02-14 09:58:16 +0530

    Fix:Error fix on suspense on import & Fixed signIn for user

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,43 @@
+import { Suspense, lazy } from "react";
 import { Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./Context/AuthContext";
-
-
-import SignIn from "./pages/SignIn/SignIn";
-import SignUp from "./pages/signup/SignUp";
-import MFA from "./pages/MFA/MFA";
-import Dashboard from "./pages/Dashboard/Dashboard";
-
-import ForgotPassword from "./pages/ForogotPassword/ForgotPassword";
-
-import ResetPassword from "./pages/ResetPassword/ResetPassword";
-import AccessDenied from "./pages/AccessDenied/AccessDenied";
-
 import RouteGuard from "./routes/RouteGuard";
 import Loader from "./components/Common/Loader/Loader";
+
+//  Lazy Loaded Pages
+const SignIn = lazy(() => import("./pages/SignIn/SignIn"));
+const SignUp = lazy(() => import("./pages/signup/SignUp"));
+const MFA = lazy(() => import("./pages/MFA/MFA"));
+const Dashboard = lazy(() => import("./pages/Dashboard/Dashboard"));
+const ForgotPassword = lazy(() => import("./pages/ForogotPassword/ForgotPassword"));
+const ResetPassword = lazy(() => import("./pages/ResetPassword/ResetPassword"));
+const AccessDenied = lazy(() => import("./pages/AccessDenied/AccessDenied"));
 
 const App = () => {
   return (
     <AuthProvider>
-      <Suspense fallback={<Loader size="lg" variant="primary" />}>
+      <Suspense
+        fallback={<Loader size="lg" variant="primary" text="Loading ..." />}
+      >
         <Routes>
 
+          {/* Public Routes */}
           <Route path="/" element={<RouteGuard requirePublic><SignIn /></RouteGuard>} />
 
-          <Route path="/signup" element={<RouteGuard requirePublic><SignUp /></RouteGuard>} />
+          <Route path="/signup" element={<RouteGuard requirePublic> <SignUp /></RouteGuard>} />
 
+          <Route path="/forgotPassword" element={<RouteGuard requirePublic><ForgotPassword /></RouteGuard>} />
+
+          {/* MFA Route */}
           <Route path="/mfa" element={<RouteGuard requireMfa><MFA /></RouteGuard>} />
 
+          {/* Protected Route */}
+          <Route path="/dashboard" element={<RouteGuard  allowedRoles={["superadmin", "admin", "user", "supervisor"]}><Dashboard /></RouteGuard>} />
 
-          <Route path="/dashboard" element={<RouteGuard requireAuth allowedRoles={["superadmin", "admin", "user", "supervisor"]}><Dashboard /></RouteGuard>} />
-          <Route path="/forgotPassword" element={<RouteGuard requirePublic><ForgotPassword /></RouteGuard>} />
+          {/* Reset Password Protected by State */}
           <Route path="/reset-password" element={<RouteGuard requireResetState><ResetPassword /></RouteGuard>} />
+
+          {/* Access Denied */}
           <Route path="/access-denied" element={<AccessDenied />} />
 
         </Routes>
